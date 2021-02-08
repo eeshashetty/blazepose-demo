139c6c460e75243ac1a9b15c96da226551a4dfe7
Eesha Shetty <eeshashetty24@gmail.com>
2021-02-08 14:18:04 +0530

    added comments

--- sketch.js
+++ sketch.js
@@ -31,12 +31,18 @@ function draw() {
     drawSkeleton();
 }
 
+// logic - find slope of the line connecting hip and knee
 function checkSquat(rk,lk,rh,lh) {
+
+    // calculate slope of right hip+knee and left hip+knee
     let a1 = Math.abs((rh.y - rk.y)/(rh.x - rk.x));
     let a2 = Math.abs((lh.y - lk.y)/(lh.x - lk.x));
     
+    // slope while standing is more than 5
     if(a1 >=5 && a2 >= 5)
         return "up"
+    
+    // slope while in squat is close to 0
     else if(a1 >=0 && a1 <=1.5 && a2 >= 0 && a1 <= 1.5)
         return "down";    
     else
@@ -57,13 +63,16 @@ function drawKeypoints()  {
         keypoint = pose.keypoints[j];
         if (keypoint.score > 0.2) {
         
+        // assign color based on whether in squat or not
         if(down)
           fill(0, 255, 0);
         else
           fill(255, 0, 0);
+
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
 
+        // save coordinates for knee and hip
         if(keypoint.part == 'rightKnee')
             rk = keypoint.position;
         else if(keypoint.part == 'leftKnee')
@@ -74,15 +83,20 @@ function drawKeypoints()  {
             lh = keypoint.position;
     }
       }
+      // continue loop if all keypoints aren't detected
       if(rk == null || lk == null || rh == null || lh == null)
-        console.log('nop');
-    else 
+        continue;
+      
+      else 
         {   
             const res = checkSquat(rk,lk,rh,lh);
             
+            // check if in squat position
             if(res == 'down')
               {down = true;}
-            else if(res == 'up')
+            
+            // check if standing - if there was a "down" right before - count as one squat
+              else if(res == 'up')
               { up = true;
                 if(down)
                   {
@@ -101,11 +115,12 @@ function drawKeypoints()  {
 function drawSkeleton() {
   for (let i = 0; i < poses.length; i++) {
     let skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
     for (let j = 0; j < skeleton.length; j++) {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
-      if(down ) {
+      
+      // assign color based on whether in squat or not
+      if(down) {
         stroke(0,255,0);
       } else {
         stroke(255,0,0);
