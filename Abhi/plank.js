fd6a95a9c75713ef035d3b4e8e2cc585dfcdc95e
Abhi Lad <42882493+abhiwolf13@users.noreply.github.com>
2021-02-19 23:52:37 +0530

    Horizontal Position Condition

--- Abhi/plank.js
+++ Abhi/plank.js
@@ -7,6 +7,7 @@ var instructstate=0;
 var t0=null;
 var t1=null;
 var plankpose=0;
+var inposition=0;
 POSE_CONNECTIONS=[[11,13],[13,15],[11,23]];
 // ==========================================================================================================
 
@@ -17,10 +18,11 @@ function Exercise(results) {
 
     var clr='blue';
     if ((results.poseLandmarks[11].visibility>0.8 && results.poseLandmarks[15].visibility>0.8 && results.poseLandmarks[13].visibility>0.8) || (results.poseLandmarks[12].visibility>0.8 && results.poseLandmarks[14].visibility>0.8 && results.poseLandmarks[16].visibility>0.8)){
-        if (results.poseLandmarks[11].y>0.5){
+        
+        if (inposition){
             a=find_angle(results.poseLandmarks[11],results.poseLandmarks[13],results.poseLandmarks[15]);
             b=find_angle(results.poseLandmarks[12],results.poseLandmarks[14],results.poseLandmarks[16])
-            if(a<120){
+            if(a<120 || b<120){
                 
                     if(t0==null || !plankpose){
                         t0=timecounter;
@@ -42,6 +44,12 @@ function Exercise(results) {
             }
 
 
+        }
+        else{
+            // Play audio
+            if(results.poseLandmarks[11].y>0.5 && (Math.abs(results.poseLandmarks[11].x-results.poseLandmarks[23].x)>0.2 || Math.abs(results.poseLandmarks[12].x-results.poseLandmarks[24].x)>0.2)){
+                inposition=1;
+            }
         }
     }
       
